088f55d16f91fdffc056db614da095abd78fd7e1
artemvoropay1 <vorapaiartem2011@gmail.com>
2025-05-24 19:31:38 +0200

    main_1.py

--- main_1.py
+++ main_1.py
@@ -1,20 +1,45 @@
-import random
+import telebot
+    
+from bot_logic import gen_pass
 
-secret_number = random.randint(1, 20)
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    bot.reply_to(message, "Привет! Я твой Telegram бот. Напиши что-нибудь!")
 
-for attempt in range(1, 6):
-    try:
-        guess = int(input(f"Попытка {attempt}: Угадай число от 1 до 20: "))
+@bot.message_handler(commands=['token'])
+def send_welcome(message):
+    bot.reply_to(message, "Иди нафиг!")
+    
+@bot.message_handler(commands=['hello'])
+def send_hello(message):
+    bot.reply_to(message, "Привет! Как дела?")
+    
+@bot.message_handler(commands=['bye'])
+def send_bye(message):
+    bot.reply_to(message, "Пока! Удачи!")
 
-        if guess == secret_number:
-            print("Поздравляю! Ты угадал число!")
-            break
-        elif guess < secret_number:
-            print("Загаданное число больше.")
-        else:
-            print("Загаданное число меньше.")
-    except ValueError:
-        print("Пожалуйста, вводи только числа.")
+@bot.message_handler(commands=['password_generate'])
+def send_bye(message):
+    bot.reply_to(message, gen_pass(7))
 
-else:
-    print(f"Попытки закончились. Было загадано число {secret_number}")
+@bot.message_handler(commands=['bir'])
+def send_bye(message):
+    bot.reply_to(message, "С днём рождения!")
+
+@bot.message_handler(commands=['alisa'])
+def send_bye(message):
+    bot.reply_to(message, "Алиса самая лучшая!")
+
+@bot.message_handler(commands=['artem'])
+def send_bye(message):
+    bot.reply_to(message, "Артём самый лучший!")
+
+@bot.message_handler(commands=['youtube'])
+def send_bye(message):
+    bot.reply_to(message, "Мой ютуб канал: https://www.youtube.com/@atemvoropayk")
+    
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+    bot.reply_to(message, message.text)
+
+bot.polling()
